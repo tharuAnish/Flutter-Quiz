562fecbcec4361ed97b89683f1d202419c88e7c5
tharuAnish <namoanishtharu@gmail.com>
2023-07-28 13:41:08 +0530

    accessing list elements and mapping list

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz/answer_button.dart';
+import 'package:quiz/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,28 +14,24 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The Question ...',
-            style: TextStyle(fontSize: 24, color: Colors.white),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(fontSize: 24, color: Colors.white),
           ),
           const SizedBox(height: 30),
-          AnswerButton(
-            onTap: () {},
-            answerText: 'Answer Text 1 ...',
-          ),
-          AnswerButton(
-            onTap: () {},
-            answerText: 'Answer Text 2 ...',
-          ),
-          AnswerButton(
-            onTap: () {},
-            answerText: 'Answer Text 3 ...',
-          ),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(
+              onTap: () {},
+              answerText: answer,
+            );
+          })
         ],
       ),
     );
